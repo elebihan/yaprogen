b64f46b50c18bef973e38da14d5f1cefee17a118
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2024-01-04 23:09:06 +0100

    disthelpers: reformat using Black

--- disthelpers.py
+++ disthelpers.py
@@ -32,7 +32,7 @@ import subprocess
 
 
 class extract_messages(cmd.Command):
-    description = 'extract localizable strings from source code'
+    description = "extract localizable strings from source code"
     user_options = []
 
     def initialize_options(self):
@@ -43,19 +43,26 @@ class extract_messages(cmd.Command):
 
     def run(self):
         domain = self.distribution.get_name()
-        potin_file = os.path.join(os.curdir, 'po', 'POTFILES.in')
-        pot_file = os.path.join(os.curdir, 'po', domain + '.pot')
+        potin_file = os.path.join(os.curdir, "po", "POTFILES.in")
+        pot_file = os.path.join(os.curdir, "po", domain + ".pot")
         args = [
-            'xgettext', '-Lpython', '-k_', '-f', potin_file, '-o', pot_file,
-            '--package-name', self.distribution.get_name(),
+            "xgettext",
+            "-Lpython",
+            "-k_",
+            "-f",
+            potin_file,
+            "-o",
+            pot_file,
+            "--package-name",
+            self.distribution.get_name(),
         ]
         subprocess.check_call(args)
 
 
 class init_catalog(cmd.Command):
-    description = 'create a new catalog based on a POT file'
+    description = "create a new catalog based on a POT file"
     user_options = [
-        ('locale=', 'l', 'locale for the new localized catalog'),
+        ("locale=", "l", "locale for the new localized catalog"),
     ]
 
     def initialize_options(self):
@@ -63,23 +70,28 @@ class init_catalog(cmd.Command):
 
     def finalize_options(self):
         if not self.locale:
-            raise DistutilsOptionError('please provide a locale')
+            raise DistutilsOptionError("please provide a locale")
 
     def run(self):
         domain = self.distribution.get_name()
-        pot_file = os.path.join(os.curdir, 'po', domain + '.pot')
-        po_file = os.path.join(os.curdir, 'po', self.locale + '.po')
+        pot_file = os.path.join(os.curdir, "po", domain + ".pot")
+        po_file = os.path.join(os.curdir, "po", self.locale + ".po")
         args = [
-            'msginit', '--input', pot_file, '--output', po_file,
-            '--locale', self.locale,
+            "msginit",
+            "--input",
+            pot_file,
+            "--output",
+            po_file,
+            "--locale",
+            self.locale,
         ]
         subprocess.check_call(args)
 
 
 class update_catalog(cmd.Command):
-    description = 'update an existing catalog from a POT file'
+    description = "update an existing catalog from a POT file"
     user_options = [
-        ('locale=', 'l', 'locale of the localized catalog'),
+        ("locale=", "l", "locale of the localized catalog"),
     ]
 
     def initialize_options(self):
@@ -87,20 +99,20 @@ class update_catalog(cmd.Command):
 
     def finalize_options(self):
         if not self.locale:
-            raise DistutilsOptionError('please provide a locale')
+            raise DistutilsOptionError("please provide a locale")
 
     def run(self):
         domain = self.distribution.get_name()
-        pot_file = os.path.join(os.curdir, 'po', domain + '.pot')
-        po_file = os.path.join(os.curdir, 'po', self.locale + '.po')
-        args = ['msgmerge', '--update', po_file, pot_file]
+        pot_file = os.path.join(os.curdir, "po", domain + ".pot")
+        po_file = os.path.join(os.curdir, "po", self.locale + ".po")
+        args = ["msgmerge", "--update", po_file, pot_file]
         subprocess.check_call(args)
 
 
 class build_catalog(cmd.Command):
-    description = 'compile *.po file into *.mo file'
+    description = "compile *.po file into *.mo file"
     user_options = [
-        ('locale=', 'l', 'locale of the localized catalog'),
+        ("locale=", "l", "locale of the localized catalog"),
     ]
 
     def initialize_options(self):
@@ -112,32 +124,32 @@ class build_catalog(cmd.Command):
     def run(self):
         locales = []
         domain = self.distribution.get_name()
-        po_dir = os.path.join(os.path.dirname(os.curdir), 'po')
+        po_dir = os.path.join(os.path.dirname(os.curdir), "po")
 
         if self.locale:
             locales.append(self.locale)
         else:
             for path, names, filenames in os.walk(po_dir):
                 for f in filenames:
-                    if f.endswith('.po'):
+                    if f.endswith(".po"):
                         locale = f[:-3]
                         locales.append(locale)
 
         for locale in locales:
-            mo_dir = os.path.join('build', 'locale', locale, 'LC_MESSAGES')
-            src = os.path.join(po_dir, locale + '.po')
-            dst = os.path.join(mo_dir, domain + '.mo')
+            mo_dir = os.path.join("build", "locale", locale, "LC_MESSAGES")
+            src = os.path.join(po_dir, locale + ".po")
+            dst = os.path.join(mo_dir, domain + ".mo")
             if not os.path.exists(mo_dir):
                 os.makedirs(mo_dir)
             print("compiling {0}".format(src))
-            args = ['msgfmt', src, '--output-file', dst]
+            args = ["msgfmt", src, "--output-file", dst]
             subprocess.check_call(args)
-            locale_dir = os.path.join('share', 'locale', locale, 'LC_MESSAGES')
+            locale_dir = os.path.join("share", "locale", locale, "LC_MESSAGES")
             self.distribution.data_files.append((locale_dir, [dst]))
 
 
 class build_man(cmd.Command):
-    description = 'build MAN page from restructuredtext'
+    description = "build MAN page from restructuredtext"
 
     def initialize_options(self):
         pass
@@ -146,29 +158,30 @@ class build_man(cmd.Command):
         pass
 
     def run(self):
-        src_dir = os.path.join(os.path.dirname(os.curdir), 'man')
-        dst_dir = os.path.join('build', 'man')
+        src_dir = os.path.join(os.path.dirname(os.curdir), "man")
+        dst_dir = os.path.join("build", "man")
         for path, names, filenames in os.walk(src_dir):
             for f in filenames:
-                if f.endswith('.rst'):
-                    filename, section, ext = f.rsplit('.', 2)
+                if f.endswith(".rst"):
+                    filename, section, ext = f.rsplit(".", 2)
                     if not os.path.exists(dst_dir):
                         os.makedirs(dst_dir)
                     src = os.path.join(path, f)
-                    dst = os.path.join(dst_dir, filename + '.' + section)
+                    dst = os.path.join(dst_dir, filename + "." + section)
                     print("converting {0}".format(src))
-                    publish_file(source_path=src,
-                                 destination_path=dst,
-                                 writer_name='manpage')
-                    man_dir = os.path.join('share', 'man', 'man' + section)
+                    publish_file(
+                        source_path=src, destination_path=dst, writer_name="manpage"
+                    )
+                    man_dir = os.path.join("share", "man", "man" + section)
                     self.distribution.data_files.append((man_dir, [dst]))
 
 
 class build(_build):
     sub_commands = _build.sub_commands
-    sub_commands += [('build_catalog', None)] + [('build_man', None)]
+    sub_commands += [("build_catalog", None)] + [("build_man", None)]
 
     def run(self):
         _build.run(self)
+
 
 # vim: ts=4 sts=4 sw=4 sta et ai
